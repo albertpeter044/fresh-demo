0205ea62a944c9043a65aa17819394115a25b232
ahuigo <1781999+ahuigo@users.noreply.github.com>
2024-03-26 18:11:08 +0800

    feat(css): add daisy plugin for taiwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,17 @@
 import { type Config } from "tailwindcss";
+import {daisyThemes} from "@/conf/daisy.ts"
+import DaisyUI from "daisyui";
 
 export default {
   content: [
     "{routes,islands,components}/**/*.{ts,tsx}",
   ],
+  plugins: [
+    // deno-lint-ignore no-explicit-any
+    DaisyUI as any,
+  ],
+  daisyui: {
+    themes: daisyThemes,
+    logs: false,
+  },
 } satisfies Config;
